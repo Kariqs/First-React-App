9132697d78a7b32a0327072df4f7ccb2fad62029
unknown <kariukibenard189@gmail.com>
2024-02-03 11:06:21 +0300

    Addad functions to collect input form data.

--- src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form className="new-expense__controls">
+    <form onSubmit={formSubmitHandler} className="new-expense__controls">
       <div className="new-expense__control">
         <label>Expense Title</label>
-        <input type="text" required />
+        <input
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+          required
+        />
       </div>
       <div className="new-expense__control">
         <label>Expense Amount</label>
-        <input type="number" min="10" step="10" required />
+        <input
+          type="number"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+          min="10"
+          step="10"
+          required
+        />
       </div>
       <div className="new-expense__control">
         <label>Expense Date</label>
-        <input type="date" min="2024-01-01" max={props.date} required />
+        <input
+          type="date"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+          min="2024-01-01"
+          max={props.date}
+          required
+        />
       </div>
       <button type="submit">Add Expense</button>
     </form>
