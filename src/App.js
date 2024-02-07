ac0ad37728adfa4f153f735eecd4cab1ae0df412
unknown <kariukibenard189@gmail.com>
2024-02-07 14:37:08 +0300

    Addded the dynamic function to store as much expenses as possible.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-
+let EXPENSES = [];
 function App() {
   const date = new Date();
-  const expenses = [
-    {
-      id: "e1",
-      date: new Date(2024, 0, 30),
-      title: "Buy Laptop",
-      price: 100000,
-    },
-    { id: "e2", date: new Date(2023, 3, 29), title: "Buy Phone", price: 25000 },
-    {
-      id: "e3",
-      date: new Date(2022, 8, 20),
-      title: "Buy Mazda CX-5",
-      price: 3500000,
-    },
-  ];
+  const [expenses, setExpenses] = useState(EXPENSES);
 
   const addExpense = (expense) => {
-    console.log("App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+     return [...prevExpenses, expense];
+    });
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,13 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <FilterExpense selected={selectedYear} onGetYear={getYear} />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        price={props.expenses[0].price}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        price={props.expenses[1].price}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        price={props.expenses[2].price}
-      />
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          date={expense.date}
+          price={expense.price}
+        />
+      ))}
     </Card>
   );
 };
